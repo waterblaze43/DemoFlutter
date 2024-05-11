57e895f45065d370e57c7f79f91b9a25f32e45af
rameez <50240050+waterblaze43@users.noreply.github.com>
2024-05-12 00:36:06 +0530

    final

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget {
+class CurrencyConverterMaterialPage extends StatefulWidget {
   const CurrencyConverterMaterialPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    const border = OutlineInputBorder(
-      borderSide: BorderSide(
-        color: Colors.black,
-        width: 2,
-        style: BorderStyle.solid,
-        strokeAlign: BorderSide.strokeAlignInside,
-      ),
-      borderRadius: BorderRadius.all(Radius.circular(5)),
-    );
+  State<CurrencyConverterMaterialPage> createState() {
+    return _CurrencyConverterMaterialPageState();
+  }
+}
 
+class _CurrencyConverterMaterialPageState
+    extends State<CurrencyConverterMaterialPage> {
+  late double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+  final border = const OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.black,
+      width: 2,
+      style: BorderStyle.solid,
+      strokeAlign: BorderSide.strokeAlignInside,
+    ),
+    borderRadius: BorderRadius.all(Radius.circular(5)),
+  );
+
+  void convert() => setState(() {
+        try {
+          result = double.parse(textEditingController.text) * 81;
+        } catch (e) {
+          result = 0;
+          debugPrint("Error");
+        }
+      });
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
       appBar: AppBar(
@@ -27,40 +47,49 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
         elevation: 0,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "0",
-              style: TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "\u{20B9} ${result != 0 ? result.toStringAsFixed(3) : result.toStringAsFixed(0)}",
+                style: const TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(10.0),
-              child: TextField(
-                style: TextStyle(color: Colors.black),
+              const SizedBox(
+                height: 60,
+              ),
+              TextField(
+                style: const TextStyle(color: Colors.black),
+                controller: textEditingController,
+                onSubmitted: (String res) {
+                  convert();
+                },
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*$'))
+                ],
                 decoration: InputDecoration(
                   hintText: "Please enter the amount in USD",
-                  hintStyle: TextStyle(color: Colors.black),
-                  prefixIcon: Icon(Icons.attach_money_rounded),
+                  hintStyle: const TextStyle(color: Colors.black),
+                  prefixIcon: const Icon(Icons.attach_money_rounded),
                   prefixIconColor: Colors.black,
                   filled: true,
                   fillColor: Colors.white,
                   focusedBorder: border,
                   enabledBorder: border,
                 ),
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextButton(
-                onPressed: () {
-                  debugPrint("hello");
-                },
+              const SizedBox(
+                height: 10,
+              ),
+              TextButton(
+                onPressed: convert,
                 style: TextButton.styleFrom(
                   backgroundColor: Colors.black,
                   foregroundColor: Colors.white,
@@ -73,8 +102,8 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                 ),
                 child: const Text("Convert"),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
